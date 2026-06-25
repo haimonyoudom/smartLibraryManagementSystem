da88acda99e6c190fe84f580a750abd475446f60
menhean0168-ctrl <menhean0168@gmail.com>
2026-06-25 16:55:39 +0700

    Update FileIO.H

---
+++ utils/FileIO.h
@@ -0,0 +1,21 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include "../util/type.h"
+#include "../linked_list/LinkedList.h"
+#include "../queue/Queue.h"
+#include "../tree/Tree.h"
+
+// books.csv  ->  id,title,author,category,qty,status,borrowCount
+void saveBooksToCSV(LinkedList& list, string filename = "data/books.csv");
+void loadBooksFromCSV(LinkedList& list, string filename = "data/books.csv");
+
+// queue.txt -> one student per line
+void saveQueueToFile(Queue& q, string filename = "data/queue.txt");
+void loadQueueFromFile(Queue& q, string filename = "data/queue.txt");
+
+// categories.txt -> one category per line
+// void saveCategoriesToFile(BST& tree, string filename = "data/categories.txt");
+// void loadCategoriesFromFile(BST& tree, string filename = "data/categories.txt");
+
+#endif
